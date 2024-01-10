1fa205876f22736ac8502aa66b1af4a1b943869c
LMan <a.hamel4480@gmail.com>
2024-01-10 12:55:13 +0100

    DataFixtures

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -2,17 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Images;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-;
+use Faker;
 
-class ImagesFixtures extends Fixture
+class ImagesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($img = 1; $img <= 30; $img++){
+            $image = new Images();
+            $image->setName($faker->imageUrl(640, 480, 'hardware', true));
+            $product = $this->getReference('prod-'.rand(1, 10));
+            $image->setProducts($product);
+            $manager->persist($image);
+        }
 
         $manager->flush();
+    }
+
+    // Execute Products avant Images
+    public function getDependencies(): array
+    {
+        return [
+            ProductsFixtures::class
+        ];
     }
 }
